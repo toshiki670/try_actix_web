f7409cb1ea548bdc1fbe9a6373397e84ee6172bf
Toshiki <bushy.trivial.0o@icloud.com>
2023-09-10 17:30:17 +0900

    Log crate 追加 'feature/log'

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,29 @@ pub mod schema;
 extern crate diesel;
 extern crate dotenv;
 
+// https://crates.io/crates/simplelog
+extern crate log;
+extern crate simplelog;
+use simplelog::*;
+use std::fs::File;
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Debug,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            File::create("log/my_rust_binary.log").unwrap(),
+        ),
+    ])
+    .unwrap();
+
     HttpServer::new(|| App::new().configure(routes::routes))
         .bind("localhost:8000")?
         .run()
