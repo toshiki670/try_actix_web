fa481f76a05ff30d4274ea32a92b01377812a322
Toshiki <bushy.trivial.0o@icloud.com>
2023-09-10 17:32:41 +0900

    アクションにLog追加 'feature/log'

--- src/controllers/users.rs
+++ src/controllers/users.rs
@@ -4,14 +4,18 @@ use crate::models::user::User;
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 
+use simplelog::*;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserData {
     name: String,
 }
 
 pub async fn create(item: web::Json<UserData>) -> HttpResponse {
+    info!("[BEGIN] create");
     let user = User::create(&(item.name));
-    println!("{:?}",user);
-    HttpResponse::Created().body("Inserting")
+    info!("Created user info: {:?}", user);
 
+    info!("[END] create");
+    HttpResponse::Created().body("Inserting")
 }
